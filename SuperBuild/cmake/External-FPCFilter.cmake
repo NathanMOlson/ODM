cb01904a342635064b77b0bc040476cb89dd4ba1
Nathan Olson <nathanmolson@gmail.com>
2025-08-26 17:35:47 -0700

    16-bit features

--- SuperBuild/cmake/External-FPCFilter.cmake
+++ SuperBuild/cmake/External-FPCFilter.cmake
@@ -7,8 +7,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/FPCFilter
-  GIT_TAG           331
+  GIT_REPOSITORY    https://github.com/NathanMOlson/FPCFilter
+  GIT_TAG           8edd50070aaf6838f4c89414b900100bb49faba0
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -25,7 +25,7 @@ ExternalProject_Add(${_proj_name}
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
   GIT_REPOSITORY    https://github.com/NathanMOlson/OpenSfM/
-  GIT_TAG           357_plus_main_2404
+  GIT_TAG           16_bit_features
   #--Update/Patch step----------
   UPDATE_COMMAND    git submodule update --init --recursive
   #--Configure step-------------
